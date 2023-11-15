ae758f648eb4d4ab698f28a18088dbc32a445ee6
amirn003 <amir.naar@gmail.com>
2023-11-16 00:10:29 +0100

    add green screen

--- scripts.js
+++ scripts.js
@@ -29,7 +29,9 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0, 0, width, height);
     // pixels = greenEffect(pixels);
-    pixels = rgbSplit(pixels);
+    // pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.1;
+    greenScreen(pixels);
     ctx.putImageData(pixels, 0, 0);
     // debugger;
   }, 16);
@@ -66,6 +68,34 @@ function rgbSplit(pixels) {
     pixels.data[i - 150] =  pixels.data[i + 2]; // Blue
 
   }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  console.log(levels);
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+
   return pixels;
 }
 
